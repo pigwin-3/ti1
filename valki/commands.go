2141c5f1687477c0268b5c262b68c5409fddbac9
pigwin <peder.vatn.austad@gmail.com>
2025-01-09 17:56:41 +0000

    hehe

--- valki/commands.go
+++ valki/commands.go
@@ -18,7 +18,8 @@ func SetValkeyValue(ctx context.Context, client valkey.Client, key, value string
 func GetValkeyValue(ctx context.Context, client valkey.Client, key string) (string, error) {
 	value, err := client.Do(ctx, client.B().Get().Key(key).Build()).ToString()
 	if err != nil {
-		return "", fmt.Errorf("failed to get value from Valkey: %v", err)
+		return "hehe", nil
+		//return "", fmt.Errorf("failed to get value from Valkey: %v", err)
 	}
 	return value, nil
 }
